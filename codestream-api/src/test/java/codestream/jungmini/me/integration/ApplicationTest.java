9a3d68203af379dca6e15e6fde21b7d6e65a8903
jungmini <jungmini0601@gmail.com>
2025-06-22 12:47:43 +0900

    [COMMON] - 공통 코드 작성 [Entity, Redis, Mysql, JPA, Querydsl, TestContainer]

---
+++ codestream-api/src/test/java/codestream/jungmini/me/integration/ApplicationTest.java
@@ -0,0 +1,11 @@
+package codestream.jungmini.me.integration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest extends IntegrationTest {
+
+    @Test
+    @DisplayName("어플리케이션이 잘 실행 되는지 확인")
+    void contextLoads() {}
+}
